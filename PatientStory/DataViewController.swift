14f3cd19ec589ec9883326a0950d4a222672d315
aritaku19@gmail.com <aritaku19@gmail.com>
2015-08-13 20:20:12 +0900

    component setting of views

---
+++ PatientStory/DataViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DataViewController.swift
+//  PatientStory
+//
+//  Created by 有村 琢磨 on 2015/08/13.
+//  Copyright (c) 2015年 有村 琢磨. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
