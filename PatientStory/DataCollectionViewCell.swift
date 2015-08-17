8e3b47709c6e341547e33f7ed0f6e21d44818018
aritaku19@gmail.com <aritaku19@gmail.com>
2015-08-18 08:18:55 +0900

    お疲れ様

---
+++ PatientStory/DataCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  DataCollectionViewCell.swift
+//  PatientStory
+//
+//  Created by 有村 琢磨 on 2015/08/18.
+//  Copyright (c) 2015年 有村 琢磨. All rights reserved.
+//
+
+import UIKit
+
+class DataCollectionViewCell: UICollectionViewCell {
+    
+}
--- PatientStory/DataViewController.swift
+++ PatientStory/DataViewController.swift
@@ -7,18 +7,41 @@
 //
 
 import UIKit
+import CoreData
 
-class DataViewController: UIViewController {
+class DataViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate{
 
+    @IBOutlet weak var navigationBar: UINavigationBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let scrollView = UIScrollView()
+        self.view.addSubview(scrollView)
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        navigationBar.barTintColor = UIColor(red: 81/256, green: 178/256, blue: 172/256, alpha: 1.0)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell :DataCollectionViewCell = collectionView.dequeueReusableCellWithReuseIdentifier("dataCell", forIndexPath: indexPath)
+        return cell
     }
     
 
